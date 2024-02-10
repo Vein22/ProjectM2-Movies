42c77b07c8b475a6f0d2f646d9897b3d54ec6fd0
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2024-02-10 01:06:50 -0400

    10-02

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,9 +2,12 @@ let solicitud = $.get("https://students-api.2.us-1.fl0.io/movies").done((data) =
    renderCards(data);
   });
   
+
   solicitud.fail(function(jqXHR, textStatus, errorThrown) {
     console.error(alert("Error al cargar datos, cargando datos de respaldo", textStatus, errorThrown));
+    renderCardsBackup()
   });
   
   const renderCards = require("./renderCards")
+  const renderCardsBackup = require("./renderCardsBackup.js")
  
