1c25fbb181a73071a892495c2be459ed6bfced7f
MMelvinMora <121642949+Vein22@users.noreply.github.com>
2024-02-15 00:49:57 -0400

    15-02

---
+++ challenge-testing/tests/CarritoCompra.test.js
@@ -0,0 +1,33 @@
+const CarritoCompra = require("../index");
+
+describe("Clase CarritoCompra", ()=>{
+
+    it("Inicializa productos con un array vacío", ()=>{
+        const carrito = new CarritoCompra();
+        expect(carrito.productos).toEqual([]);
+    });
+
+    it("Agregar un producto al carrito", ()=>{
+        const carrito = new CarritoCompra();
+        const producto = {nombre: "Producto 1", precio: 10};
+        carrito.agregarProducto(producto);
+        expect(carrito.productos.length).toBe(1);
+        expect(carrito.productos[0]).toBe(producto);
+    });
+
+    it("Calcular el total de la compra", ()=>{
+        const carrito = new CarritoCompra();
+        carrito.agregarProducto({nombre: "Producto 1", precio: 10});
+        carrito.agregarProducto({nombre: "Producto 2", precio: 20});
+        expect(carrito.calcularTotal()).toBe(30);
+    });
+
+    it("Aplicar un descuento a la compra", ()=>{
+        const carrito = new CarritoCompra();
+        carrito.agregarProducto({nombre: "Producto 1", precio: 100});
+        carrito.agregarProducto({nombre: "Producto 2", precio: 200});
+        expect(carrito.aplicarDescuento(10)).toBe(270);
+    });
+});
+
+  
